b001138ecd56bf3b87e58137f1899cd0227a2a43
lucky-rathore <luckyrathore03@gmail.com>
2022-04-26 09:53:38 +0530

    calling using google sheet

--- commonUtil.js
+++ commonUtil.js
@@ -1,7 +1,7 @@
 const header = `
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="./about-us.html">HRPF</a>
+      <a class="navbar-brand">HRPF</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
