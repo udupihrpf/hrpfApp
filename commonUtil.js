66d50354f9fd9bdc4b1b049047d25c6d24e5a6e5
lucky-rathore <luckyrathore03@gmail.com>
2022-05-07 13:45:20 +0530

    update icon

--- commonUtil.js
+++ commonUtil.js
@@ -1,7 +1,7 @@
 const header = `
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#930606;">
-      <a href="/">
+      <a href="https://lucky-rathore.github.io/hrpfApp/">
       <img src="https://scontent.fblr24-1.fna.fbcdn.net/v/t1.18169-9/1016653_486159911472626_1994311130_n.png?_nc_cat=101&amp;ccb=1-6&amp;_nc_sid=09cbfe&amp;_nc_ohc=pjHyPBprH-wAX919NLY&amp;_nc_ht=scontent.fblr24-1.fna&amp;oh=00_AT_MkPOt1FE6W4sEPNCYaWhmw2MagEIeowUNNoEPKQOKOQ&amp;oe=629D62DE" weight="50px" class="navbar-brand" style="background: #930606" height="50px">
       </a>
       <button class="navbar-toggler mr-1" type="button" data-toggle="collapse" data-target="#navbarCollapse"
