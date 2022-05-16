388a8db70cf7e31baedfe8660883a249b50d1243
lucky-rathore <luckyrathore03@gmail.com>
2022-05-16 17:05:19 +0530

    logo, header color

--- commonUtil.js
+++ commonUtil.js
@@ -1,16 +1,15 @@
 const header = `
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#930606;">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#f73b3b;">
       <a href="https://lucky-rathore.github.io/hrpfApp/">
-      <img src="https://scontent.fblr24-1.fna.fbcdn.net/v/t1.18169-9/1016653_486159911472626_1994311130_n.png?_nc_cat=101&amp;ccb=1-6&amp;_nc_sid=09cbfe&amp;_nc_ohc=pjHyPBprH-wAX919NLY&amp;_nc_ht=scontent.fblr24-1.fna&amp;oh=00_AT_MkPOt1FE6W4sEPNCYaWhmw2MagEIeowUNNoEPKQOKOQ&amp;oe=629D62DE" weight="50px" class="navbar-brand" style="background: #930606" height="50px">
+      <img src="assets/img/hrpf-logo.jpeg" width="50px" height="50px">
       </a>
       <button class="navbar-toggler mr-1" type="button" data-toggle="collapse" data-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          
+        <ul class="navbar-nav mr-auto">          
           <li class="nav-item dropdown active">
             <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">Our Work<span class="sr-only">(current)</span></a>
